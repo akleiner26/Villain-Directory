46cc4ec3008b3784f1c25e7eb5fabed774269d52
orangeblack13 <AndrewJKleiner@gmail.com>
2020-08-02 18:48:05 -0400

    table format changed

--- src/components/Table/table.js
+++ src/components/Table/table.js
@@ -6,16 +6,29 @@ import villains from "../../villains.json"
 function Table(props) {
     return (
         <table>
-            <tr>
-                <th>Photo</th>
-                <th>Name</th>
-                <th>First Appearance</th>
-                <th>Alias</th>
-                <th>Abilities</th>
-            </tr>
-            <tr>
-                {villains.map(character => <VillainRow image={character.image} name={character.name} firstAppearance={character.firstAppearance} alias={character.alias} ability={character.ability}/>)}
-            </tr>
+            <thead>
+                <tr>
+                    <th>Photo</th>
+                    <th>Name</th>
+                    <th>First Appearance</th>
+                    <th>Alias</th>
+                    <th>Abilities</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{villains.map(character => <VillainRow 
+                    image={character.image} />)}</td> 
+                    <td>{villains.map(character => <VillainRow 
+                    name={character.name} />)}</td>
+                    <td>{villains.map(character => <VillainRow 
+                    firstAppearance={character.firstAppearance} />)}</td>
+                    <td>{villains.map(character => <VillainRow 
+                    alias={character.alias} />)}</td>
+                    <td>{villains.map(character => <VillainRow 
+                    ability={character.ability} />)}</td>
+                </tr>
+            </tbody>
         </table>
     )
 }
