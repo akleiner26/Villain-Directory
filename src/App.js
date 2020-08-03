f781eb6b5035cde86e5d617db5a2a2edcdda155a
orangeblack13 <AndrewJKleiner@gmail.com>
2020-08-03 10:09:33 -0400

    input search added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Header from "./components/Header/header";
 import Wrapper from "./components/Wrapper/wrapper";
 import Table from "./components/Table/table";
+import Search from "./components/Search/search";
 
 function App() {
   return (
     <>
       <Header></Header>
       <Wrapper>
+        <Search>
+        </Search>
         <Table>
         </Table>
       </Wrapper>
---
+++ src/components/Search/search.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./search.css";
+
+function Search(props) {
+return(
+     <input type="text" className="input" placeholder="Search Villain" />
+);
+}
+
+export default Search;
