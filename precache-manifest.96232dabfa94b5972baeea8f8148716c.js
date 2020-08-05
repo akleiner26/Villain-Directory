1fcbb28c809e36b629dfe013bd31727708d2a291
orangeblack13 <AndrewJKleiner@gmail.com>
2020-08-05 19:46:07 -0400

    Updates

---
+++ precache-manifest.96232dabfa94b5972baeea8f8148716c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83bf99cc2985767a2e4091c5478f0d03",
+    "url": "/Villain-Directory/index.html"
+  },
+  {
+    "revision": "e88fe6026e0aafb5eae2",
+    "url": "/Villain-Directory/static/css/main.131c660a.chunk.css"
+  },
+  {
+    "revision": "70d3d1f83dfb0abf45d5",
+    "url": "/Villain-Directory/static/js/2.538bbeeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Villain-Directory/static/js/2.538bbeeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e88fe6026e0aafb5eae2",
+    "url": "/Villain-Directory/static/js/main.30a94297.chunk.js"
+  },
+  {
+    "revision": "71687c9435438b222aef",
+    "url": "/Villain-Directory/static/js/runtime-main.93f74e60.js"
+  },
+  {
+    "revision": "f687ca2c884eb76fc08d74d80354bab0",
+    "url": "/Villain-Directory/static/media/batmfa__.f687ca2c.ttf"
+  },
+  {
+    "revision": "3e0475a82addd1fc75beaa84bd32f6e7",
+    "url": "/Villain-Directory/static/media/gotham.3e0475a8.jpg"
+  }
+]);
